d67d93c896ef9cc7d4d98da849cf4cfb3ceb7cd9
luboshh <luboshhh@gmail.com>
2021-10-04 14:12:36 +0200

    Prohození a a b

--- HelloWorld/HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -6,28 +6,19 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Toto je prvni prikaz ktery se vykona po spusteni aplikace.");
-            int sum = Add(5, 3);
-            Console.WriteLine(sum);
-            AddAndPrint(4, 9);
-            Hello("Lubomir");
+            int a = 20;
+            int b = 30;
+
+
+            Console.WriteLine($"a: {a}, b: {b}");
+
+            var A = b;
+            var B = a;
+
+            Console.WriteLine($"a: {A}, b: {B}");
+
         }
 
-        static void Hello(string name)
-        {
-            Console.WriteLine($"Hello {name}");
-        }
 
-        static int Add(int a, int b)
-        {
-            
-            return a + b;
-        }
-        static int AddAndPrint(int a, int b)
-        {
-            int sum = a + b;
-            Console.WriteLine($"vysledek souctu {a} + {b} = {sum}");
-            return a + b;
-        }
     }
 }
