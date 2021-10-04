6d62b4854054c835ca22e4fbee6a7b3fa1a02871
luboshh <luboshhh@gmail.com>
2021-10-04 12:05:03 +0200

    Pokus 1

--- HelloWorld/HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,26 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Toto je prvni prikaz ktery se vykona po spusteni aplikace.");
+            Hello();
+            int sum = Add(5, 3);
+            Console.WriteLine(sum);
+            int sum2 = AddAndPrint(4, 9);
+        }
+
+        static int AddAndPrint(int a,int b)
+        {
+            return a + b;
+        }
+
+        static void Hello()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        static int Add(int a, int b)
+        {
+            return a + b;
         }
     }
 }
