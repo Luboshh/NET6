28004275f02122eaa7a315bad6026c95bc887d1d
luboshh <luboshhh@gmail.com>
2021-10-04 12:25:04 +0200

    Vypsání jména do konzole

--- HelloWorld/HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -7,27 +7,26 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Toto je prvni prikaz ktery se vykona po spusteni aplikace.");
-            Hello();
             int sum = Add(5, 3);
             Console.WriteLine(sum);
             AddAndPrint(4, 9);
+            Hello("Lubomir");
         }
 
-        static int AddAndPrint(int a,int b)
+        static void Hello(string name)
         {
-            int sum = a + b;
-            Console.WriteLine($"vysledek souctu {a} + {b} = {sum}");
-            return a + b;
-        }
-
-        static void Hello()
-        {
-            Console.WriteLine("Hello");
+            Console.WriteLine($"Hello {name}");
         }
 
         static int Add(int a, int b)
         {
             
+            return a + b;
+        }
+        static int AddAndPrint(int a, int b)
+        {
+            int sum = a + b;
+            Console.WriteLine($"vysledek souctu {a} + {b} = {sum}");
             return a + b;
         }
     }
