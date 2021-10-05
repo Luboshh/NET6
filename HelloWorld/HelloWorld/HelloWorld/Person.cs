07a4e2b37b843d36a007d2259e79fff67045e754
luboshh <luboshhh@gmail.com>
2021-10-05 11:45:56 +0200

    Person class a property metoda

---
+++ HelloWorld/HelloWorld/HelloWorld/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+           
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+
+        }
+}
--- HelloWorld/HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -6,62 +6,17 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            // úkol:
-            // metodu (switch case) která dostance na vstup
-            // číslo 1 - 7 a vrátí název dne pondělí...neděle
 
-            var daynumber = 5;
-            var dayname = GetDayName(daynumber);
-            Console.WriteLine($"{daynumber}. den je {dayname}");
+            Person p1 = new Person();
+            p1.FirstName = "Adam";
+            p1.LastName = "Smith";
+            p1.Age = 30;
+
+            Console.WriteLine($"Osoba 1: {p1.GetFullName()}");
+
         }
 
-        static string GetDayName(int day)
-        {
-            switch (day)
-            {
-                case 1:
-                    return "pondělí";
-                    break;
-                case 2:
-                    return "úterý";
-                    break;
-                case 3:
-                    return "středa";
-                    break;
-                case 4:
-                    return "čtvrtek";
-                    break;
-                case 5:
-                    return "pátek";
-                    break;
-                case 6:
-                    return "sobota";
-                    break;
-                case 7:
-                    return "neděle";
-                    break;
-                default:
-                    return "neznámý den";
-                    break;
-            }
-        }
 
-        static string GetGenerationName(int year)
-        {
-            if (year > 1946 && year <= 1964)
-                return "Baby Boomer";
-            else if (year >= 1965 && year <= 1980)
-                return "Gen X";
-            else if (year >= 1981 && year <= 1996)
-            {
-                return "Millenial";
-            }
-            else if (year >= 1997 && year <= 2012)
-                return "Gen Z";
-            else if (year >= 2013)
-                return "Gen Alpha";
-            else
-                return $"neznám název generace pro rok {year}";
-        }
+
     }
 }
