d5480cc0a553f3801c5bc85d3586f6318cdf5bd5
luboshh <luboshhh@gmail.com>
2021-10-05 10:02:36 +0200

    Určení dne v týdnu

--- HelloWorld/HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -9,35 +9,45 @@ namespace HelloWorld
             // úkol:
             // uživatel zadá rok narození a program
             // mu vypíše do jaké patří generace
+            // metodu (switch case) která dostance na vstup
+            // číslo 1 - 7 a vrátí název dne pondělí...neděle
 
-            // Baby Boomerss — born 1946 - 1964
-            // Generation X — born 1965 - 1980
-            // Millenials — born 1981 - 1996
-            // Generation Z — born 1997 - 2012
-            // Generation Alpha — born 2013 -
-
-            Console.WriteLine("Zadejte svůj rok narození: ");
-            string input = Console.ReadLine();
-            var year = int.Parse(input);
-
-            var gen_urceni = UrceniGenerace(year);
-            Console.WriteLine($"Gen: {gen_urceni}");
+            var daynumber = 5;
+            var dayname = GetDayName(daynumber);
+            Console.WriteLine($"{daynumber}. den je {dayname}");
         }
-
-        static string UrceniGenerace(int year)
+         static string GetDayName(int day)
         {
-            if (year > 1946 && year < 1964)
-            return "Baby Boomer";
-            if (year > 1965 && year < 1980)
-            return "Generation X";
-            if (year > 1981 && year < 1996)
-            return "Millenials";
-            if (year > 1997 && year < 2021)
-            return "Generation Z";
-            if (year >= 2013)
-            return "Gen Aplpha";
-            else
-                return $"Neznám název generace pro rok {year}";
+            switch (day)
+            {
+                case 1:
+                    return "pondělí";
+                    break;
+                case 2:
+                    return "úterý";
+                    break;
+                case 3:
+                    return "středa";
+                    break;
+                case 4:
+                    return "čtvrtek";
+                    break;
+                case 5:
+                    return "pátek";
+                    break;
+                case 6:
+                    return "sobota";
+                    break;
+                case 7:
+                    return "neděle";
+                    break;
+                default:
+                    return "neznámý den";
+                    break;
+            }
         }
+
+        
     }
+
 }
