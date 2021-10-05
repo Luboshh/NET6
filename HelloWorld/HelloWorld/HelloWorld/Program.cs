9ea2ccbb29189173c543d8e862a3538db55442cd
luboshh <luboshhh@gmail.com>
2021-10-05 13:40:34 +0200

    Souřadnice GPS

--- HelloWorld/HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -6,8 +6,8 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point("10.234", "234.3403");
-            Console.WriteLine($"Souřadnice GPS: {p1.GetFullGPS()}");
+            var Souradnice1 = new Point("10.234", "234.3403");
+            Console.WriteLine($"Souřadnice GPS: {Souradnice1.GetFullGPS()}");
             //Person p1 = new Person("Adam", "Smith", 30);
             //Console.WriteLine($"Osoba 1: {p1.GetFullName()}");
         }
