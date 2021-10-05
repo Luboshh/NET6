63e0a6dbf1a4439f895b9cc6d0b01eca42498ff9
luboshh <luboshhh@gmail.com>
2021-10-05 12:03:26 +0200

    Konstruktor - věk

--- HelloWorld/HelloWorld/HelloWorld/Person.cs
+++ HelloWorld/HelloWorld/HelloWorld/Person.cs
@@ -8,6 +8,13 @@ namespace HelloWorld
 {
     class Person
     {
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
         public string FirstName { get; set; }
            
         public string LastName { get; set; }
