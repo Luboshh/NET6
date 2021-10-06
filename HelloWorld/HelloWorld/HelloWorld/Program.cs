372a1412556deb766caf3536787dc42bc234506c
luboshh <luboshhh@gmail.com>
2021-10-06 12:13:47 +0200

    Středa_filtrování pole (where, count, sum...)

--- HelloWorld/HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -11,28 +11,58 @@ namespace HelloWorld
         {
             int[] numbers = { -2079, -498, 2920, -1856, 332, -2549, -674, -120, -992, 2782, 320, -524, 135, 952, 1868, 2509, -230, -138, -904, -480 };
 
-            /// z "numebers" zjistěte:
+            /// z "numbers" zjistěte:
             /// 1. počet prvků v poli
-            /// 2. největší hodnotu
-            /// 3. nejmenší hodnotu
-            /// 4. průměr
-            /// 5. kolik obsahuje pole kladných čísel
-            /// 6. kolik obsahuje pole záporných čísel
-            /// 7. sumu všech hodnot
-            /// 8. sumu kladných hodnot
+            var count = numbers.Count();
+            Console.WriteLine("počet prvků v poli " + count);
 
+            /// 2. největší hodnotu
+            var max = numbers.Max();
+            Console.WriteLine("největší hodnota " + max);
+
+            /// 3. nejmenší hodnotu
+            var min = numbers.Min();
+            Console.WriteLine("nejmenší hodnota " + min);
+
+            /// 4. průměr
+            var average = numbers.Average();
+            Console.WriteLine("průměr " + average);
+
+            /// 5. kolik obsahuje pole kladných čísel
+            var count_positive = numbers.Where(x => x >= 0).Count();
+            Console.WriteLine("kladných čísel " + count_positive);
+
+            /// 6. kolik obsahuje pole záporných čísel
+            var count_negative = numbers.Where(x => x < 0).Count();
+            Console.WriteLine("záporných čísel " + count_negative);
+
+            /// 7. sumu všech hodnot
+            var sum = numbers.Sum();
+            Console.WriteLine("suma: " + sum);
+
+            /// 8. sumu kladných hodnot
+            var sum_positive = numbers.Where(x => x >= 0).Sum();
+            Console.WriteLine("suma kladnych: " + sum_positive);
 
             ////// projection / restrikce / filtrovani - Where
 
             /// 9. všechna čísla větší než -500
+            //var result = numbers.Where(x => x > -500);
+
             /// 10. všechna kladná sudá čísla
-            /// 11. čísla v rozstahu -400 až 400
+            //var result = numbers.Where(x => x >= 0)
+            //                    .Where(x => x % 2 == 0);
+
+            /// 11. čísla v rozsahu -400 až 400
+            var result = numbers.Where(x => x > -400 && x < 400);
 
 
-            //foreach (var item in ordered)
-            //{
-            //    Console.WriteLine(item);
-            //}
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
         }
 
         static List<Person> LoadPeople(string file)
